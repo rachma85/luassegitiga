7244294545cc2345f47b05b0fe7beca684c6cefb
rachma85 <120188967+rachma85@users.noreply.github.com>
2025-03-14 10:20:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ st.write(
     "Aplikasi ini untuk menghitung luas segitiga."
 )
 alas = st.number_input("Masukkan nilai alas (cm)")
-st.write("Alas= ", number)
+st.write("Alas= ", alas)
 tinggi = st.number_input("Masukkan tinggi (cm)")
-st.write("Tinggi= ", number)
+st.write("Tinggi= ", tinggi)
 Luas=0,5*alas*tinggi
 st.write("Luas segitiga=",Luas)
